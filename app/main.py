604005784fb7f71a0e9725cf6ecf50cd1f3e5b2c
Ilya <ila9600550@gmail.com>
2026-02-17 15:31:38 +0000

    fix8

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
+from typing import List
+
+
 class Car:
     def __init__(
-        self,
-        comfort_class: int,
-        clean_mark: int,
-        brand: str
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
     ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
@@ -12,21 +15,22 @@ class Car:
 
 class CarWashStation:
     def __init__(
-        self,
-        distance_from_city_center: float,
-        clean_power: int,
-        average_rating: float,
-        count_of_ratings: int
+            self,
+            distance_from_city_center: float,
+            clean_power: int,
+            average_rating: float,
+            count_of_ratings: int
     ) -> None:
-        self.distance_from_city_center = (
-            distance_from_city_center if distance_from_city_center != 0
-            else 1.0
-        )
+        if distance_from_city_center == 0:
+            # Захист від ділення на нуль
+            distance_from_city_center = 1.0
+
+        self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
 
-    def serve_cars(self, cars: list[Car]) -> float:
+    def serve_cars(self, cars: List[Car]) -> float:
         result_amount = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -37,9 +41,9 @@ class CarWashStation:
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
             price = (
-                car.comfort_class
-                * (self.clean_power - car.clean_mark)
-                * self.average_rating / self.distance_from_city_center
+                    car.comfort_class
+                    * (self.clean_power - car.clean_mark)
+                    * self.average_rating / self.distance_from_city_center
             )
             return round(price, 1)
         return 0.0
