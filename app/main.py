a13739f96afb7d892a4bc5d9864cbcdf600331c0
Ilya <ila9600550@gmail.com>
2026-02-17 15:16:19 +0000

    fix4

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(
+            self,
+            comfort_class: int,
+            clean_mark: int,
+            brand: str
+    ) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
@@ -19,7 +24,7 @@ class CarWashStation:
         self.count_of_ratings = count_of_ratings
 
     def serve_cars(self, cars: list[Car]) -> float:
-        result_amount = 0
+        result_amount = 0.0
         for car in cars:
             if car.clean_mark < self.clean_power:
                 result_amount += self.calculate_washing_price(car)
@@ -28,9 +33,11 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
-            price = (car.comfort_class
-                     * (self.clean_power - car.clean_mark)
-                     * self.average_rating / self.distance_from_city_center)
+            price = (
+                    car.comfort_class
+                    * (self.clean_power - car.clean_mark)
+                    * self.average_rating / self.distance_from_city_center
+            )
             return round(price, 1)
         return 0.0
 
